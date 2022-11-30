9854daccd3eb01f01987420eed1d8d93e122774b
Fernanda Bollinger <feernanda.bollinger@gmail.com>
2022-11-30 16:01:08 -0300

    feat:add styles and structure

--- src/pages/notFound/index.jsx
+++ src/pages/notFound/index.jsx
@@ -1,1 +1,24 @@
-export const NotFound = () => {};
+import notFoundImg from "../../assets/notFound.png";
+import { StyledSection } from "./styles";
+
+import { useNavigate } from "react-router-dom";
+
+export const NotFound = () => {
+  const navigate = useNavigate();
+
+  const backToLogin = (e) => {
+    e.preventDefault();
+
+    navigate("/");
+  };
+
+  return (
+    <StyledSection>
+      <img src={notFoundImg} alt="Page not found" />
+      <h1>Page not found.</h1>
+      <button type="submit" onClick={backToLogin}>
+        Voltar a página de login
+      </button>
+    </StyledSection>
+  );
+};
